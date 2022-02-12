dc81c9713a18d1d9a9faa6b0b8fa1019d0d1905b
Firelods <35725423+Firelods@users.noreply.github.com>
2022-02-12 01:05:17 +0100

    voldec

--- js/menuderoulantresponsive.js
+++ js/menuderoulantresponsive.js
@@ -1,23 +1,37 @@
-
-
 function myFunction() {
     var x = document.getElementById("navi");
 
-        if (x.style.opacity == 0) {
-            x.style.opacity = 1;
-            x.style.pointerEvents="all";
-            
-        } else {
-            x.style.opacity = 0;
-            x.style.pointerEvents="none";
-            console.log("test");
-            // sleep(500);
-            
-            // x.style.display = "none";
-        }
+    if (x.style.opacity == 0) {
+        x.style.opacity = 1;
+        x.style.pointerEvents = "all";
 
-    
+    } else {
+        x.style.opacity = 0;
+        x.style.pointerEvents = "none";
+        console.log("test");
+        // sleep(500);
 
-        // x.style.opacity = 1;
-    
+        // x.style.display = "none";
+    }
+
+
+
+    // x.style.opacity = 1;
+
+}
+
+function myFunction1() {
+
+    var x = document.getElementById("prix1");
+    var x1 = document.getElementById("prix");
+    if (x.style.opacity==0) {
+        x.style.opacity = 1;
+    } else {
+        x.style.opacity = 0;
+    }
+    if (x1.style.opacity==0) {
+        x1.style.opacity = 1;
+    } else {
+        x1.style.opacity = 0;
+    }
 }
